ed71e5fdb3f8ab9c9a24dec810353eaf818f7341
nasaownsky <someuser.ivn@gmail.com>
2022-03-16 17:59:15 +0300

    Init

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,88 @@
+import React, { useRef, useEffect } from "react";
+import { createPortal } from "react-dom";
+import clsx from "clsx";
+import useOnClickOutside from "use-onclickoutside";
+
+import styles from "./Modal.scss";
+
+type Props = {
+  isOpen: boolean;
+  width: string;
+  height: string;
+  overlayClassName: string;
+  modalClassName: string;
+  closeButton: boolean;
+  closeOnClickOutside: boolean;
+  isCentered: boolean;
+  onClose: () => void;
+};
+
+const Modal: React.FC<Props> = ({
+  isOpen,
+  width,
+  height,
+  overlayClassName,
+  modalClassName,
+  closeButton = true,
+  closeOnClickOutside = true,
+  isCentered = true,
+  onClose,
+  children,
+}) => {
+  const ref = useRef();
+  useOnClickOutside(ref, closeOnClickOutside && onClose);
+
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? "hidden" : "";
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return createPortal(
+    <div className={styles.modal}>
+      <div
+        className={clsx(
+          styles.modal__overlay,
+          overlayClassName && overlayClassName
+        )}
+      />
+      <div
+        className={clsx(
+          styles.modal__wrapper,
+          isCentered && styles.modal__wrapper_centered
+        )}
+        aria-modal
+        aria-hidden
+        aria-label="modal"
+        tabIndex={-1}
+        role="dialog"
+      >
+        <div
+          className={clsx(styles.modal__body, modalClassName && modalClassName)}
+          style={{ width: width, height: height }}
+          ref={ref}
+        >
+          {closeButton && (
+            <button
+              type="button"
+              className={styles.modal__closeButton}
+              data-dismiss="modal"
+              aria-label="close"
+              onClick={onClose}
+            />
+          )}
+          <div className={styles.modal__content}>{children}</div>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
+};
+
+export default Modal;
